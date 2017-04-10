1d687e474f3b9c3ced115dfe86de4796b429bbb0
Lauri Kasanen <cand@gmx.com>
2017-04-10 14:25:49 +0300

    Implement yearly vert lines

--- stockchart.cpp
+++ stockchart.cpp
@@ -3,6 +3,8 @@
 
 void stockchart::draw() {
 
+	u32 i;
+
 	u32 bx, by, bw, bh;
 	bx = x() + 5;
 	by = y() + 5;
@@ -24,6 +26,8 @@ void stockchart::draw() {
 	if (!src)
 		return;
 
+	const u32 maxsize = src->size();
+
 	dx += 48;
 	dw -= 48;
 
@@ -37,6 +41,53 @@ void stockchart::draw() {
 	dy++;
 	dw -= 2;
 	dh -= 2;
+
+	// Vertical day/month lines
+	bool yearly = false;
+	if ((*src)[0].year - (*src)[maxsize - 1].year > 1)
+		yearly = true;
+
+	char tmp[32];
+	int tw, th;
+
+	fl_font(FL_HELVETICA, 14);
+	const float pointw = dw / (float) maxsize;
+	if (yearly) {
+		for (i = 1; i < maxsize; i++) {
+			const stockval &cur = (*src)[i - 1];
+			const stockval &prev = (*src)[i];
+
+			if (cur.month != prev.month) {
+				const u32 x = dx + (dw - i * pointw);
+
+				if (cur.year != prev.year) {
+					fl_color(FL_BLACK);
+
+					sprintf(tmp, "%u", cur.year);
+					fl_measure(tmp, tw, th, 0);
+					fl_draw(tmp, x - tw / 2,
+						dy + dh + 6 - fl_descent() + fl_height());
+
+					fl_color(FL_GRAY0 + 9);
+				} else {
+					fl_color(FL_GRAY0 + 18);
+
+					fl_font(FL_HELVETICA, 11);
+					sprintf(tmp, "%u", cur.month);
+					fl_measure(tmp, tw, th, 0);
+					fl_draw(tmp, x - tw / 2,
+						dy + dh + 1 - fl_descent() + fl_height());
+
+					fl_font(FL_HELVETICA, 14);
+				}
+
+				// Draw line here
+				fl_line(x, dy, x, dy + dh - 2);
+			}
+		}
+	} else {
+		fl_color(FL_GRAY0 + 18);
+	}
 }
 
 void stockchart::setsource(const std::vector<stockval> * const vec, const float tgt) {
